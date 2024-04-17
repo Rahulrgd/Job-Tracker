65c1b6f9a09ae0053212d7d84c4279ec027f7ad6
RahulGupta <rahulrgd21@gmail.com>
2024-04-17 21:44:01 +0530

    Job Tracker - Project Started

---
+++ src/test/java/com/rahul/job_tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.job_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
