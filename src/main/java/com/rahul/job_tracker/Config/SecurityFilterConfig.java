26da5f007d4197a2183547a35fd39e0228b523b4
RahulGupta <rahulrgd21@gmail.com>
2024-04-28 09:17:43 +0530

    Step: Externalised CORs origin from for different evironment, also Deleted 2 useless folders.

--- src/main/java/com/rahul/job_tracker/Config/SecurityFilterConfig.java
+++ src/main/java/com/rahul/job_tracker/Config/SecurityFilterConfig.java
@@ -4,6 +4,7 @@ import com.rahul.job_tracker.JwtAuthentication.JwtAuthenticationFilter;
 import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,15 +26,25 @@ public class SecurityFilterConfig {
   @Autowired
   private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+  private String allowedOrigin;
+
+  public SecurityFilterConfig() {}
+
+  public SecurityFilterConfig(
+    @Value("${allowed.origin}") String allowedOrigin
+  ) {
+    this.allowedOrigin = allowedOrigin;
+  }
+
+  public String getOrigin() {
+    return allowedOrigin;
+  }
+
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    // configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-    configuration.setAllowedOrigins(
-      Arrays.asList(
-        "http://job-tracker-and-manager.s3-website.eu-north-1.amazonaws.com"
-      )
-    );
+    // configuration.setAllowedOrigins(Arrays.asList(getOrigin()));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
     configuration.setAllowedMethods(Arrays.asList("*"));
     configuration.setAllowedHeaders(Arrays.asList("*"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
